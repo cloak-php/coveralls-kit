5835b24fe2cf48bf1f8b6b9f8e0bde1fec44a1c9
holyshared <holy.shared.design@gmail.com>
2014-03-21 18:31:54 +0900

    add example

---
+++ Phakefile
@@ -0,0 +1,8 @@
+<?php
+
+group('example', function() {
+    desc('Run the example program basic');
+    task('basic', function() {
+        require_once __DIR__ . '/example/basic_example.php';
+    });
+});
---
+++ example/basic_example.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace coveralls\example;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use coveralls\JSONFileBuilder;
+use coveralls\jsonfile\Coverage;
+use coveralls\jsonfile\SourceFile;
+
+
+/**
+ * Get the code coverage
+ */
+xdebug_start_code_coverage(XDEBUG_CC_UNUSED | XDEBUG_CC_DEAD_CODE);
+
+require_once __DIR__ . '/basic/example.php';
+
+$result = xdebug_get_code_coverage();
+xdebug_stop_code_coverage();
+
+
+/**
+ * Generate a json file
+ */
+$builder = new JSONFileBuilder();
+$builder->token('foo');
+
+foreach ($result as $file => $coverage) {
+    if (preg_match('/vendor/', $file) || preg_match('/src/', $file)) {
+        continue;
+    }
+
+    $source = new SourceFile($file);
+    $coverages = $source->getCoverages();
+
+    foreach ($coverage as $line => $status) {
+        if ($status === 1) {
+            $coverages->add(Coverage::executed(1));
+        } else if ($status === -1) {
+            $coverages->add(Coverage::unused(1));
+        }
+    }
+}
+
+$builder->addSource($source);
+$builder->build()->saveAs(__DIR__ . '/tmp/coverage.json');
