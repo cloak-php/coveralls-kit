7905e7730107238559fd2ff771386c33cdf91457
holyshared <holy.shared.design@gmail.com>
2014-03-22 19:58:14 +0900

    add RemoteSpec

---
+++ spec/jsonfile/repository/RemoteSpec.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of CoverallsKit.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace coveralls\spec;
+
+use coveralls\jsonfile\repository\Remote;
+
+describe('Remote', function() {
+    before(function() {
+        $this->remote = new Remote('origin', 'https://github.com/holyshared/coveralls-kit.git');
+    });
+    describe('__toString', function() {
+        it('should return string value', function () {
+            $expect = json_encode($this->remote->toArray());
+            $value = (string) $this->remote;
+            expect($value)->toEqual($expect);
+        });
+    });
+});
