67066d6c7ebee5c9e2f39c10418e2e18c852332d
holyshared <holy.shared.design@gmail.com>
2014-03-23 15:20:42 +0900

    add coveralls

--- script/coveralls.php
+++ script/coveralls.php
@@ -5,7 +5,7 @@ namespace coveralls\example;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use coveralls\JSONFileBuilder;
-use coveralls\entity\service\TravisCI;
+use coveralls\entity\service\Travis;
 use coveralls\entity\Coverage;
 use coveralls\entity\Repository;
 use coveralls\entity\SourceFile;
@@ -29,7 +29,7 @@ xdebug_stop_code_coverage();
  */
 $builder = new JSONFileBuilder();
 $builder->token('N852hqDzBRTjy2U9hxQ0HzGblXC9ASCTQ')
-    ->service(TravisCI::ci())
+    ->service(Travis::travisCI())
     ->repository(new Repository(__DIR__ . '/../'));
 
 foreach ($result as $file => $coverage) {
