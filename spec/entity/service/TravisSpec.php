ab6aab1e19a02f70a6a26ec2b8b09d99bf6a7beb
holyshared <holy.shared.design@gmail.com>
2014-07-21 22:50:44 +0900

    Update spec

--- spec/entity/service/TravisSpec.php
+++ spec/entity/service/TravisSpec.php
@@ -15,20 +15,7 @@ use coverallskit\entity\service\Travis;
 
 describe('Travis', function() {
     before(function() {
-        $this->jobId = getenv(Travis::ENV_JOB_ID);
-        if (empty($this->jobId)) {
-            putenv(Travis::ENV_JOB_ID . '=10');
-        }
-
-        $this->coverallsToken = getenv(Travis::ENV_COVERALLS_REPO_TOKEN_KEY);
-        if (empty($this->coverallsToken)) {
-            putenv(Travis::ENV_COVERALLS_REPO_TOKEN_KEY . '=token');
-        }
-
         $this->service = new Travis();
-    });
-    after(function () {
-        putenv(Travis::ENV_COVERALLS_REPO_TOKEN_KEY);
     });
     describe('isEmpty', function() {
         context('when service name is empty', function() {
@@ -38,19 +25,9 @@ describe('Travis', function() {
             });
         });
     });
-    describe('getServiceJobId', function() {
-        it('should return job id', function() {
-            expect($this->service->getServiceJobId())->not()->toBeEmpty();
-        });
-    });
     describe('getServiceName', function() {
         it('should return the service name', function() {
             expect($this->service->getServiceName())->toEqual('travis-ci');
-        });
-    });
-    describe('getCoverallsToken', function() {
-        it('should return the coveralls api token', function() {
-            expect($this->service->getCoverallsToken())->not()->toBeEmpty();
         });
     });
     describe('travisCI', function() {
