1e6c0dc7db935c55e5f36b0acf942aa83f10e0b7
holyshared <holy.shared.design@gmail.com>
2014-07-21 15:02:48 +0900

    spec refactaring

--- spec/ConfigurationSpec.php
+++ spec/ConfigurationSpec.php
@@ -15,41 +15,46 @@ use coverallskit\Configuration;
 use Prophecy\Prophet;
 
 describe('Configuration', function() {
-    before(function() {
-        $this->prophet = new Prophet();
 
-        $this->service = $this->prophet->prophesize('coverallskit\entity\service\ServiceInterface');
-        $this->service->getServiceJobId()->shouldNotBeCalled();
-        $this->service->getServiceName()->shouldNotBeCalled();
+    describe('__construct', function() {
+        context('when specify the attribute', function() {
+            before(function() {
+                $this->prophet = new Prophet();
 
-        $this->configration = new Configuration([
-            'name' => 'coveralls.json',
-            'token' => 'api-token',
-            'service' => $this->service->reveal(),
-            'repositoryDirectory' => __DIR__
-        ]);
-    });
-    after(function() {
-        $this->prophet->checkPredictions();
-    });
-    describe('getName', function() {
-        it('should return report file name', function() {
-            expect($this->configration->getName())->toEqual('coveralls.json');
+                $this->service = $this->prophet->prophesize('coverallskit\entity\service\ServiceInterface');
+                $this->service->getServiceJobId()->shouldNotBeCalled();
+                $this->service->getServiceName()->shouldNotBeCalled();
+
+                $this->configration = new Configuration([
+                    'name' => 'coveralls.json',
+                    'token' => 'api-token',
+                    'service' => $this->service->reveal(),
+                    'repositoryDirectory' => __DIR__
+                ]);
+            });
+            after(function() {
+                $this->prophet->checkPredictions();
+            });
+            it('should set the name', function() {
+                expect($this->configration->getName())->toEqual('coveralls.json');
+            });
+            it('should set the coveralls api token', function() {
+                expect($this->configration->getToken())->toEqual('api-token');
+            });
+            it('should set the service instance', function() {
+                expect($this->configration->getService())->toBeAnInstanceOf('\coverallskit\entity\service\ServiceInterface');
+            });
+            it('should set the repository directory', function() {
+                expect($this->configration->getRepositoryDirectory())->toEqual(__DIR__);
+            });
+        });
+        context('when specify an attribute that does not exist', function() {
+            it('should throw coverallskit\exception\BadAttributeException', function() {
+                expect(function() {
+                    new Configuration([ 'does not exist' => 'foo' ]);
+                })->toThrow('coverallskit\exception\BadAttributeException');
+            });
         });
     });
-    describe('getToken', function() {
-        it('should return coveralls api token', function() {
-            expect($this->configration->getToken())->toEqual('api-token');
-        });
-    });
-    describe('getService', function() {
-        it('should return service instance', function() {
-            expect($this->configration->getService())->toBeAnInstanceOf('\coverallskit\entity\service\ServiceInterface');
-        });
-    });
-    describe('getRepositoryDirectory', function() {
-        it('should return repository directory', function() {
-            expect($this->configration->getRepositoryDirectory())->toEqual(__DIR__);
-        });
-    });
+
 });
