fbfb5fc3dd358a83fd1ef284f1908c82cf1dc3c7
holyshared <holy.shared.design@gmail.com>
2014-07-21 15:21:00 +0900

    add configration loader

---
+++ spec/ConfigurationLoaderSpec.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * This file is part of CoverallsKit.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace coverallskit\spec;
+
+use coverallskit\ConfigurationLoader;
+
+describe('ConfigurationLoader', function() {
+
+    describe('loadFromFile', function() {
+        before(function() {
+            $this->loader = new ConfigurationLoader();
+        });
+        context('when the file exists', function() {
+            it('should return coverallskit\Configuration instance', function() {
+                $config = $this->loader->loadFromFile(__DIR__ . '/fixtures/coveralls.yml');
+                expect($config)->toBeAnInstanceOf('coverallskit\Configuration');
+            });
+        });
+        context('when the file not exists', function() {
+            it('should throw NotFoundConfigurationFileException', function() {
+                expect(function() {
+                    $this->loader->loadFromYamlFile(__DIR__ . '/fixtures/not_found_coveralls.yml');
+                })->toThrow('coverallskit\exception\ConfigurationFileNotExistException');
+            });
+        });
+    });
+
+    describe('loadFromYamlFile', function() {
+        before(function() {
+            $this->loader = new ConfigurationLoader();
+        });
+        context('when the file exists', function() {
+            it('should return coverallskit\Configuration instance', function() {
+                $config = $this->loader->loadFromYamlFile(__DIR__ . '/fixtures/coveralls.yml');
+                expect($config)->toBeAnInstanceOf('coverallskit\Configuration');
+            });
+        });
+        context('when the file not exists', function() {
+            it('should throw coverallskit\exception\ConfigurationFileNotExistException', function() {
+                expect(function() {
+                    $this->loader->loadFromYamlFile(__DIR__ . '/fixtures/not_found_coveralls.yml');
+                })->toThrow('coverallskit\exception\ConfigurationFileNotExistException');
+            });
+        });
+    });
+
+});
---
+++ src/ConfigurationLoader.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This file is part of CoverallsKit.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace coverallskit;
+
+/**
+ * Class ConfigurationLoader
+ * @package coverallskit
+ */
+class ConfigurationLoader implements ConfigurationLoaderInterface
+{
+
+    /**
+     * @param string $file
+     * @return Configuration
+     */
+    public function loadFromFile($file)
+    {
+    }
+
+    /**
+     * @param string $file
+     * @return Configuration
+     */
+    public function loadFromYamlFile($file)
+    {
+    }
+
+}
