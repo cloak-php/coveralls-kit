9eae450b9a2e221fdabafccbc9a89fcb200e6792
holyshared <holy.shared.design@gmail.com>
2014-07-26 16:32:35 +0900

    Remove const

--- src/entity/service/TravisInterface.php
+++ src/entity/service/TravisInterface.php
@@ -14,9 +14,6 @@ namespace coverallskit\entity\service;
 interface TravisInterface extends ServiceInterface
 {
 
-    const SERVICE_CI = 'travis-ci';
-    const SERVICE_PRO = 'travis-pro';
-
     const ENV_JOB_ID = 'TRAVIS_JOB_ID';
 
 }
