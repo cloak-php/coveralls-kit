5c10f5b048a1f72d75fac4802d3fc762b3b1e835
holyshared <holy.shared.design@gmail.com>
2014-07-26 16:15:06 +0900

    Refactaring

--- spec/ReportBuilderSpec.php
+++ spec/ReportBuilderSpec.php
@@ -17,18 +17,15 @@ use coverallskit\entity\repository\Commit;
 use coverallskit\entity\repository\Branch;
 use coverallskit\entity\repository\Remote;
 use coverallskit\entity\collection\RemoteCollection;
-use Prophecy\Prophet;
 use Mockery;
 
 describe('ReportBuilder', function() {
 
     describe('build', function() {
         before(function() {
-            $this->prophet = new Prophet();
-
-            $this->service = $this->prophet->prophesize('coverallskit\entity\service\TravisInterface');
-            $this->service->getServiceJobId()->shouldBeCalled()->willReturn('10');
-            $this->service->getServiceName()->shouldBeCalled()->willReturn('travis-ci');
+            $this->service = Mockery::mock('coverallskit\entity\service\TravisInterface');
+            $this->service->shouldReceive('getServiceJobId')->andReturn('10');
+            $this->service->shouldReceive('getServiceName')->andReturn('travis-ci');
 
             $this->commit = new Commit([
                 'id' => '3fdcfa494f3e9bcb17f90085af9d11a936a7ef4e',
@@ -48,10 +45,10 @@ describe('ReportBuilder', function() {
             ]);
             $this->remotes = new RemoteCollection([ $remote ]);
 
-            $this->repository = $this->prophet->prophesize('coverallskit\entity\RepositoryInterface');
-            $this->repository->getCommit()->willReturn($this->commit);
-            $this->repository->getBranch()->willReturn($this->branch);
-            $this->repository->getRemotes()->willReturn($this->remotes);
+            $this->repository = Mockery::mock('coverallskit\entity\RepositoryInterface');
+            $this->repository->shouldReceive('getCommit')->andReturn($this->commit);
+            $this->repository->shouldReceive('getBranch')->andReturn($this->branch);
+            $this->repository->shouldReceive('getRemotes')->andReturn($this->remotes);
 
             $this->foo = realpath(__DIR__ . '/fixtures/foo.php');
             $this->bar = realpath(__DIR__ . '/fixtures/bar.php');
@@ -59,15 +56,15 @@ describe('ReportBuilder', function() {
             $this->builder = new ReportBuilder();
             $this->builder->reportFilePath(__DIR__  . '/tmp/coverage.json');
             $this->builder->token('foo');
-            $this->builder->repository($this->repository->reveal());
-            $this->builder->service($this->service->reveal());
+            $this->builder->repository($this->repository);
+            $this->builder->service($this->service);
             $this->builder->addSource(new SourceFile($this->foo));
             $this->builder->addSource(new SourceFile($this->bar));
 
             $this->report = $this->builder->build();
         });
         after(function() {
-            $this->prophet->checkPredictions();
+            Mockery::close();
         });
         it('should same as that specifies the name of the result report', function() {
             expect($this->report->name)->toBe(__DIR__  . '/tmp/coverage.json');
