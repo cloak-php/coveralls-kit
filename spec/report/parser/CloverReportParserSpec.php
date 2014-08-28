91c040c7010b4897faf6831a83f303d81c640251
holyshared <holy.shared.design@gmail.com>
2014-08-28 18:13:45 +0900

    add parser sources

---
+++ spec/report/parser/CloverReportParserSpec.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of CoverallsKit.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace coverallskit\spec\report\parser;
+
+use coverallskit\report\parser\CloverReportParser;
+
+describe('CloverReportParser', function() {
+    describe('parseReport', function() {
+        it('return coverage result');
+    });
+});
