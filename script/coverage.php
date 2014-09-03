24142919647209b8c5e2e03b70033c45c39c8ab5
holyshared <holy.shared.design@gmail.com>
2014-09-03 09:22:15 +0900

    add coverage script

---
+++ script/coverage.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace coverallskit\example;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use PHP_CodeCoverage;
+use PHP_CodeCoverage_Report_Clover;
+use PHP_CodeCoverage_Filter;
+
+$filter = new PHP_CodeCoverage_Filter();
+$filter->addDirectoryToWhitelist('src');
+
+$coverage = new PHP_CodeCoverage(null, $filter);
+$reporter = new PHP_CodeCoverage_Report_Clover();
+
+$coverage->start('default');
+
+$argv = ['../vendor/bin/pho'];
+require_once __DIR__ . "/../vendor/bin/pho";
+
+$coverage->stop();
+
+$reporter->process($coverage, 'script/clover.xml');
