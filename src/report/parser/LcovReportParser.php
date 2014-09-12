330b310a15f0df205b110a8b34519c8774278a81
holyshared <holy.shared.design@gmail.com>
2014-09-12 09:30:47 +0900

    add code

--- src/report/parser/LcovReportParser.php
+++ src/report/parser/LcovReportParser.php
@@ -11,7 +11,12 @@
 
 namespace coverallskit\report\parser;
 
+use coverallskit\entity\SourceFile;
+use coverallskit\entity\collection\SourceFileCollection;
+use coverallskit\entity\Coverage;
+use coverallskit\entity\collection\CoverageCollection;
 use coverallskit\report\ReportParserInterface;
+use coverallskit\exception\ExceptionCollection;
 
 /**
  * Class LcovReportParser
@@ -32,6 +37,38 @@ class LcovReportParser implements ReportParserInterface
     public function parse($reportContent)
     {
         $this->reportContent = $reportContent;
+        $lines = explode(PHP_EOL, $this->reportContent);
+
+        $sourceFile = null;
+        $sourceCollection = new SourceFileCollection();
+        $parseErrors = new ExceptionCollection();
+        $coverages = [];
+
+        foreach ($lines as $line) {
+            if (preg_match('/FN:/', $line) === 1) {
+                $name = preg_replace('/^FN:(.+)$/', '', $line);
+                $sourceFile = new SourceFile($name);
+                $coverages = [];
+            } else if (preg_match('/end_of_record/', $line) === 1) {
+                try {
+                    $sourceFile->getCoverages()->addAll($coverages);
+                } catch (ExceptionCollection $exception) {
+                    $parseErrors = $exception;
+                }
+                $sourceCollection->add($sourceFile);
+            } else if (preg_match('/DA:/', $line) === 1) {
+                $line = preg_replace('/DA:/', '', $line);
+                $params = explode(',', $line);
+                list($lineNumber, $executeCount) = $params;
+
+                $lineNumber = (int) $lineNumber;
+                $analysisResult = ((int) $executeCount >= 1) ? Coverage::EXECUTED : Coverage::UNUSED;
+
+                $coverages[] = new Coverage($lineNumber, $analysisResult);
+            }
+        }
+
+        return new Result($sourceCollection, $parseErrors);
     }
 
 }
