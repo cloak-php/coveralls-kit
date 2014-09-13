88ab8ca58f0588deb8c541708e5b836c55e5b3f0
holyshared <holy.shared.design@gmail.com>
2014-09-13 17:48:46 +0900

    add coverage script

--- script/coverage.php
+++ script/coverage.php
@@ -4,21 +4,36 @@ namespace coverallskit\example;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use PHP_CodeCoverage;
-use PHP_CodeCoverage_Report_Clover;
-use PHP_CodeCoverage_Filter;
 
-$filter = new PHP_CodeCoverage_Filter();
-$filter->addDirectoryToWhitelist('src');
+use cloak\Analyzer;
+use cloak\ConfigurationBuilder;
+use cloak\Result\File;
+use cloak\reporter\CompositeReporter;
+use cloak\reporter\LcovReporter;
+use cloak\reporter\ProcessingTimeReporter;
+use cloak\reporter\TextReporter;
 
-$coverage = new PHP_CodeCoverage(null, $filter);
-$reporter = new PHP_CodeCoverage_Report_Clover();
 
-$coverage->start('default');
+$analyzer = Analyzer::factory(function(ConfigurationBuilder $builder) {
+
+    $reporter = new CompositeReporter([
+        new LcovReporter(__DIR__ . '/report.lcov'),
+        new TextReporter(),
+        new ProcessingTimeReporter()
+    ]);
+
+    $builder->includeFile(function(File $file) {
+        return $file->matchPath('/src');
+    })->excludeFile(function(File $file) {
+        $matchExcludeTarget = $file->matchPath('/spec') || $file->matchPath('/vendor');
+        return $matchExcludeTarget;
+    })->reporter($reporter);
+
+});
+
+$analyzer->start();
 
 $argv = ['../vendor/bin/pho', '--stop'];
 require_once __DIR__ . "/../vendor/bin/pho";
 
-$coverage->stop();
-
-$reporter->process($coverage, 'script/clover.xml');
+$analyzer->stop();
