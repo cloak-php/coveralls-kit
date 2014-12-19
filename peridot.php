95373cf59f887fba663e1fa3bbb6954b80ac6c19
holyshared <holy.shared.design@gmail.com>
2014-12-19 13:22:21 +0900

    use peridot-cloak

--- peridot.php
+++ peridot.php
@@ -2,8 +2,10 @@
 
 use Evenement\EventEmitterInterface;
 use expectation\peridot\ExpectationPlugin;
+use cloak\peridot\CloakPlugin;
 
 return function(EventEmitterInterface $emitter)
 {
     ExpectationPlugin::create()->registerTo($emitter);
+    CloakPlugin::create('cloak.toml')->registerTo($emitter);
 };
