a8edd4d820c6dfe43fbdfd3a46044205acb18a12
holyshared <holy.shared.design@gmail.com>
2014-12-20 13:59:45 +0900

    add basic example

--- RoboFile.php
+++ RoboFile.php
@@ -16,4 +16,15 @@ class RoboFile extends Tasks
         return $this->taskExec($peridot . ' ' . $peridotSpecTargets)->run();
     }
 
+    public function exampleBasic()
+    {
+        $runtime = 'php';
+        $enviromentValue = 'TRAVIS_JOB_ID=10';
+        $exampleScript = __DIR__ . '/example/basic_example.php';
+
+        $command = sprintf('%s %s %s', $enviromentValue, $runtime, $exampleScript);
+
+        return $this->taskExec($command)->run();
+    }
+
 }
