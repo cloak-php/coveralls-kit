0363a26a7593325a48747457dafd020890d1006e
holyshared <holy.shared.design@gmail.com>
2014-12-22 13:13:17 +0900

    add sources and spec

---
+++ spec/environment/TravisPro.spec.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of CoverallsKit.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace coverallskit\spec;
+
+use coverallskit\Environment;
+use coverallskit\environment\TravisPro;
+
+describe('TravisPro', function() {
+    describe('#isSupported', function() {
+        context('when supported', function() {
+            beforeEach(function() {
+                $environment = new Environment([
+                    'CI' => 'true',
+                    'TRAVIS' => 'true',
+                    'TRAVIS_JOB_ID' => '10',
+                    'COVERALLS_REPO_TOKEN' => 'token'
+                ]);
+                $this->travisPro = new TravisPro($environment);
+            });
+            it('return true', function() {
+                expect($this->travisPro->isSupported())->toBeTruthy();
+            });
+        });
+    });
+});
