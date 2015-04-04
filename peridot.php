403c919ee519f2380e18bfdd3780ac977d46c768
Noritaka Horio <holy.shared.design@gmail.com>
2015-04-04 21:42:04 +0900

    Upgrade packages

--- peridot.php
+++ peridot.php
@@ -1,14 +1,14 @@
 <?php
 
 use Evenement\EventEmitterInterface;
-use expectation\peridot\ExpectationPlugin;
-use Peridot\Reporter\Dot\DotReporterPlugin;
+use expect\peridot\ExpectPlugin;
 use cloak\peridot\CloakPlugin;
+use Peridot\Reporter\Dot\DotReporterPlugin;
 
 return function(EventEmitterInterface $emitter)
 {
     $dot = new DotReporterPlugin($emitter);
-    ExpectationPlugin::create()->registerTo($emitter);
+    ExpectPlugin::create()->registerTo($emitter);
 
     if (defined('HHVM_VERSION') === false) {
         CloakPlugin::create('.cloak.toml')->registerTo($emitter);
